a5c67ee09813850821ee1b2920955608825fb6d8
Iciclez <0iciclez0@gmail.com>
2020-11-13 23:53:56 -0500

    0

---
+++ Organizer/Windows/InputWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Organizer.Windows
+{
+    /// <summary>
+    /// Interaction logic for InputWindow.xaml
+    /// </summary>
+    public partial class InputWindow : Window
+    {
+        public enum Results
+        {
+            OK = 1,
+            Cancel = 2
+        }
+
+        public string Prompt { get; private set; }
+
+        public string Response { get; private set; }
+
+        public Results Result { get; private set; } 
+
+        public InputWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string InputBox(string prompt, string title = "", string defaultResponse = "")
+        {
+            InputWindow inputWindow = new InputWindow
+            {
+                Title = title,
+                Prompt = prompt,
+                Response = defaultResponse,
+                Result = Results.Cancel
+            };
+
+            inputWindow.ShowDialog();
+
+            return inputWindow.Result == Results.OK ? inputWindow.Response : string.Empty; 
+        }
+
+        private void inputWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Prompt;
+            textBox.Text = Response;
+            textBox.Focus();
+        }
+
+        private void textBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                okButton_Click(sender, null);
+            }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            Result = Results.OK;
+            Response = textBox.Text;
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Result = Results.Cancel;
+            Response = null;
+            Close();
+        }
+    }
+}
---
+++ Organizer/Windows/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Organizer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DirectorySelectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && Directory.Exists(folderBrowserDialog.SelectedPath))
+            {
+                DirectoryTextBox.Text = folderBrowserDialog.SelectedPath.ToLowerInvariant();
+            }
+        }
+
+        private void AddMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string extension = InputWindow.InputBox("enter an extension: (*.*)", "organizer");
+
+            if (string.IsNullOrEmpty(extension))
+            {
+                return;
+            }
+
+            if (extension.Contains("(") || extension.Contains(")"))
+            {
+                extension = extension.Replace("(", "").Replace(")", "");
+            }
+
+            if (!extension.Contains("."))
+            {
+                extension = "." + extension;
+            }
+
+            if (!extension.Contains("*"))
+            {
+                extension = "*" + extension;
+            }
+
+            ExtensionListBox.Items.Add(new ListBoxItem { Content = extension });
+        }
+
+        private void RemoveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ExtensionListBox.Items.Remove(ExtensionListBox.SelectedItem);
+        }
+
+        private void ClearMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ExtensionListBox.Items.Clear();
+        }
+
+        private void OrganizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string prefix = (bool)AddPrefixCheckBox.IsChecked ? PrefixTextBox.Text : "";
+            string suffix = (bool)AddSuffixCheckBox.IsChecked ? SuffixTextBox.Text : "";
+            DirectoryInfo directoryInfo = new DirectoryInfo(DirectoryTextBox.Text);
+
+            if (!int.TryParse(StartingIndexTextBox.Text, out int startIndex))
+            {
+                startIndex = 0;
+            }
+
+            int i = startIndex;
+
+            if (FileExtensionsCheckBox.IsChecked == true && ExtensionListBox.Items.Count > 0)
+            {
+                foreach (ListBoxItem lbi in ExtensionListBox.Items)
+                {
+                    FileInfo[] files = directoryInfo.GetFiles(lbi.Content.ToString());
+                    foreach (FileInfo file in files)
+                    {
+                        file.MoveTo(Path.Combine(DirectoryTextBox.Text, prefix + i++ + suffix + file.Extension));
+                    }
+
+                    if (ResetIndexCheckBox.IsChecked == true && ResetIndexCheckBox.IsEnabled == true)
+                    {
+                        i = startIndex;
+                    }
+                }
+                
+            }
+            else
+            {
+                FileInfo[] files = directoryInfo.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    file.MoveTo(Path.Combine(DirectoryTextBox.Text, prefix + i++ + suffix + file.Extension));
+                }
+            }
+        }
+    }
+}
